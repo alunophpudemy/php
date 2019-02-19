3024c344c4c0e7d3eb5598f7de91b54e17935871
alunophpudemy <47426718+alunophpudemy@users.noreply.github.com>
2019-02-19 11:30:16 -0300

    Add files via upload

---
+++ exemplo-02.php
@@ -0,0 +1,29 @@
+<?php 
+
+//$anoNascimento = 1990;
+
+$nome1 = "Wendel";
+
+$sobrenome = "Neves";
+
+$nomeCompleto = $nome1 ." " . $sobrenome;
+
+echo $nomeCompleto;
+
+exit;
+
+
+//Na linha de baixo termo uma ariavel com numero no nome
+echo $nome1;
+
+echo "<br/>";
+
+unset($nome1);
+
+	if(isset($nome1)){
+
+	echo $nome1;
+
+	}
+
+?>
---
+++ exemplo-03.php
@@ -0,0 +1,22 @@
+<?php 
+
+$nome = "Hcode";
+$site = 'www.hcode.com.br';
+
+$ano= 1990;
+//////////////////
+$frutas = array("abacaxi","laranja","uva");
+
+//echo $frutas[2];
+
+//var_dump($nascimento);
+
+//////////////////////////////
+
+$arquivo = fopen("exemplo-03.php","r");
+
+var_dump($arquivo)
+//$nulo = NULL;
+
+?>
+
